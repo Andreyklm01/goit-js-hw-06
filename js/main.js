b66a8b885062bd6f8b41543a49c620d9bfcf19fa
Andrey Klemenko <web.klemenko@gmail.com>
2021-01-18 23:13:35 +0200

    complete task 10

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
 import users from './users.js';
 
-//task-1
+//     ====task-1====
 const getUserNames = users => {
   return users.map(user => user.name);
 };
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-//task-2
+//      =====task-2=====
 const getUsersWithEyeColor = (users, color) => {
   return users.filter(user => user.eyeColor === color);
 };
 console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-//task-3
+//        =====task-3======
 const getUsersWithGender = (users, gender) => {
   return users.filter(user => user.gender === gender).map(user => user.name);
 };
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-//task-4 получить массив неактивных пользователей
+//        =====task-4=====
+//получить массив неактивных пользователей
 const getInactiveUsers = users => {
   return users.filter(user => !user.isActive);
 };
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-//task-5
+//        =====task-5=====
 // Получить пользоваля (не массив) по email (поле email, он уникальный).
 const getUserWithEmail = (users, email) => {
   return users.find(user => user.email === email);
@@ -33,7 +34,7 @@ const getUserWithEmail = (users, email) => {
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
-//task-6
+//        =====task-6=====
 //Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
 const getUsersWithAge = (users, min, max) => {
   return users.filter(user => user.age >= min && user.age <= max);
@@ -42,23 +43,25 @@ console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, об
 console.log(getUsersWithAge(users, 30, 40));
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-//task-7
+//        =====task-7=====
 //Получить общую сумму баланса (поле balance) всех пользователей.
 const calculateTotalBalance = users => {
   return users.reduce((acc, user) => acc + user.balance, 0);
 };
 console.log(calculateTotalBalance(users)); // 20916
 
-//task-8
+//        =====task-8=====
 //Массив имен всех пользователей у которых есть друг с указанным именем.
 const getUsersWithFriend = (users, friendName) => {
-  const filtered = users.filter(user => user.friends.includes(friendName));
-  return filtered.map(user => user.name);
+  const filteredUsers = users
+    .filter(user => user.friends.includes(friendName))
+    .map(user => user.name);
+  return filteredUsers;
 };
 console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-//task-9
+//        =====task-9=====
 //Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
 const getNamesSortedByFriendsCount = users => {
   const usersCopyArr = [...users];
@@ -69,4 +72,18 @@ const getNamesSortedByFriendsCount = users => {
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
-//task-10
+//        =====task-10=====
+//Получить массив всех умений всех пользователей (поле skills),
+//при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+const getSortedUniqueSkills = users => {
+  const allSkills = users
+    .flatMap(user => user.skills)
+    .filter((item, index, arr) => {
+      return arr.indexOf(item) === index;
+    })
+    .sort();
+  return allSkills;
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
